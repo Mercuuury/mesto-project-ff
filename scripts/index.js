a9dc000ed521c12fa0d4cee988ce3811d74002dd
Mercuuury <kovshiksa@gmail.com>
2024-12-29 14:04:57 +0300

    complete the basic functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector(".places__list"); // Контейнер для вставки карточек
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardContent, onDelete) {
+  // 1. Клонируем темплейт
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  // 2. Выбираем нужные элементы внутри карточки
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Вывести карточки на страницу
+  // 3. Заполняем карточку данными
+  cardTitle.textContent = cardContent.name;
+  cardImage.src = cardContent.link;
+  cardImage.alt = cardContent.name;
+
+  // 4. Вешаем обработчик для удаления карточки
+  deleteButton.addEventListener("click", () => {
+    onDelete(cardElement);
+  });
+
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Вывести карточки на страницу
+initialCards.forEach((item) => {
+  const newCard = createCard(item, deleteCard);
+  placesList.append(newCard);
+});
